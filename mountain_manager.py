63208582a61dd04e406e4997d856a35afcc18542
zhenghengtan <140365770+zhenghengtan@users.noreply.github.com>
2023-09-30 15:20:42 +0800

    Update mountain_manager.py
    errrrmmmmm not sure whether it fulfill the requirement the one in ed too short i hope its right since it passes the test in my perspective
    
    Signed-off-by: zhenghengtan <140365770+zhenghengtan@users.noreply.github.com>

--- mountain_manager.py
+++ mountain_manager.py
@@ -3,20 +3,55 @@ from mountain import Mountain
 
 class MountainManager:
 
+    # def __init__(self) -> None:
+    #     pass
+
+    # def add_mountain(self, mountain: Mountain) -> None:
+    #     raise NotImplementedError()
+
+    # def remove_mountain(self, mountain: Mountain) -> None:
+    #     raise NotImplementedError()
+
+    # def edit_mountain(self, old: Mountain, new: Mountain) -> None:
+    #     raise NotImplementedError()
+
+    # def mountains_with_difficulty(self, diff: int) -> list[Mountain]:
+    #     raise NotImplementedError()
+
+    # def group_by_difficulty(self) -> list[list[Mountain]]:
+    #     raise NotImplementedError()
+        
     def __init__(self) -> None:
-        pass
+        self.mountains = []
 
     def add_mountain(self, mountain: Mountain) -> None:
-        raise NotImplementedError()
+        self.mountains.append(mountain)
 
     def remove_mountain(self, mountain: Mountain) -> None:
-        raise NotImplementedError()
+        if mountain in self.mountains:
+            self.mountains.remove(mountain)
 
     def edit_mountain(self, old: Mountain, new: Mountain) -> None:
-        raise NotImplementedError()
+        if old in self.mountains:
+            index = self.mountains.index(old)
+            self.mountains[index] = new
 
     def mountains_with_difficulty(self, diff: int) -> list[Mountain]:
-        raise NotImplementedError()
+        return [mountain for mountain in self.mountains if mountain.difficulty_level == diff]
 
     def group_by_difficulty(self) -> list[list[Mountain]]:
-        raise NotImplementedError()
+         # Create a dictionary to group mountains by difficulty
+        grouped_mountains = {}
+        for mountain in self.mountains:
+            difficulty = mountain.difficulty_level
+            if difficulty not in grouped_mountains:
+                grouped_mountains[difficulty] = []
+            grouped_mountains[difficulty].append(mountain)
+
+        # Sort the groups by difficulty in ascending order
+        sorted_groups = sorted(grouped_mountains.items(), key=lambda x: x[0])
+
+        # Extract the lists of mountains from the sorted groups
+        result = [group[1] for group in sorted_groups]
+        return result
+
